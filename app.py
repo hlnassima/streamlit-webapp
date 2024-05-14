86628cc87129f7434982b411e5bbaeee826dbb2a
hlnassima <110915364+hlnassima@users.noreply.github.com>
2024-05-14 14:12:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-image: python:3.8
+import streamlit as st
 
-stages:
-  - deploy
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
-deploy:
-  stage: deploy
-  script:
-    - pip install -r requirements.txt
-    - streamlit run app.py 
+#header section
+st.subheader("Hi, I am Sven :wave:")
+st.title("A Data Analyst From Germany")
+st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings.")
+st.write("[Learn More >](https://pythonandvba.com)")
